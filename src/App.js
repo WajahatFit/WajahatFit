fe9ce1e96b480348651af9b9d40645baadeb0e72
Syed Wajahat Ali <wajahat.klk25.com>
2023-05-10 22:06:12 +0100

    added all the files back

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contact from "./components/Contact";
+import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Start from "./components/Start";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="scroll-smooth">
+      <Toaster />
+      <NavBar />
+      <Start />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
